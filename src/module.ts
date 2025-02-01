6f3f301737dd2b51fcb474ed312a5b7ad5c59e35
Alex Mihaylichenko <alex.mihaa@gmail.com>
2025-02-01 13:41:58 +0300

    6.x-dev: переделал провайдер

--- src/module.ts
+++ src/module.ts
@@ -1,24 +1,18 @@
-import {ModuleWithProviders, NgModule} from '@angular/core';
+import {EnvironmentProviders, makeEnvironmentProviders} from '@angular/core';
 import {provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
 
 import {JSON_API_BASE_URL, JsonApiParamsParser, JsonApiStore, JsonApiStoreAdapter, JsonApiUrlBuilder} from './services';
 import {JsonApiDocumentSerializer, JsonApiResourceSerializer} from './serializers';
 
-@NgModule({})
-export class JsonApiModule {
-  public static forRoot(apiUrl: string): ModuleWithProviders<JsonApiModule> {
-    return {
-      ngModule: JsonApiModule,
-      providers: [
-        provideHttpClient(withInterceptorsFromDi()),
-        {provide: JSON_API_BASE_URL, useValue: apiUrl},
-        JsonApiResourceSerializer,
-        JsonApiDocumentSerializer,
-        JsonApiUrlBuilder,
-        JsonApiParamsParser,
-        JsonApiStoreAdapter,
-        JsonApiStore
-      ]
-    };
-  }
+export function provideJsonApi(apiUrl: string): EnvironmentProviders {
+  return makeEnvironmentProviders([
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: JSON_API_BASE_URL, useValue: apiUrl },
+    JsonApiResourceSerializer,
+    JsonApiDocumentSerializer,
+    JsonApiUrlBuilder,
+    JsonApiParamsParser,
+    JsonApiStoreAdapter,
+    JsonApiStore,
+  ]);
 }
